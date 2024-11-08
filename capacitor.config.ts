8d2e5ccddb1623cf11196a17c8e241fabfb99b6b
Oulis Nikos <oulisnikos@gmail.com>
2024-11-08 21:07:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OSS-Open-fitness',
+  webDir: 'www'
+};
+
+export default config;
